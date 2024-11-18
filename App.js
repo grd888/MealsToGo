317d8f00b13bfbb9707bbdd27358b74ab8cb7144
Greg Delgado <grd888@gmail.com>
2024-11-18 14:43:28 +0800

    use SafeAreaView for iOS and StatusBar.currentheight for Android

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, Platform } from 'react-native';
 
+const isAndroid = Platform.OS === 'android';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+      <View style={{ backgroundColor: 'green', padding: 16 }}>
+        <Text>search</Text>
+      </View>
+      <View style={{ backgroundColor: 'blue', flex: 1, padding: 16 }}>
+        <Text>list</Text>
+      </View>
+    </SafeAreaView>
+    <ExpoStatusBar style='auto'/>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
